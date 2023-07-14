28faba2a09edfbf094b33407a1ce5bf1390751e7
WilsonPiedrahita <wilsonferney2013@gmail.com>
2023-07-13 23:14:49 -0500

    archivo 4

---
+++ 4-only_diff_elements.py
@@ -0,0 +1,17 @@
+def only_diff_elements(set_1, set_2):
+    
+    unique_elements = set()
+
+  
+    for element in set_1:
+        
+        if element not in set_2:
+            unique_elements.add(element)
+
+   
+    for element in set_2:
+        
+        if element not in set_1:
+            unique_elements.add(element)
+
+    return unique_elements
